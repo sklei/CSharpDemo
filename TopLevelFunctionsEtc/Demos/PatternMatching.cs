c10fccf191d387561f8c232cb211bd3dbf8cee3a
Steven van Klei <steven.vanklei@onefox.nl>
2022-01-26 01:22:25 +0100

    Some extra changes

--- TopLevelFunctionsEtc/Demos/PatternMatching.cs
+++ TopLevelFunctionsEtc/Demos/PatternMatching.cs
@@ -7,6 +7,29 @@ public class PatternMatchingDemo : IDemo
 	public void DoDemo()
 	{
 		WriteLine("- Smarties ----------------------");
+		var countSmarty = new SmartyWithCount("Red", 420);
+		var overlordSmarty = new SmartyOverlord("PureBlack");
+
+		void HandleSmarty(Smarty s)
+		{
+			if(s is SmartyWithCount smc) {
+				WriteLine($"Has a count! {smc.Count}");
+			} else if(s is SmartyOverlord os) {
+				WriteLine($"Is {os.Color} overlord");
+			}
+
+			if(s is SmartyOverlord { Color: "PureBlack" }) {
+				WriteLine("😎");
+			}
+
+			if(s is SmartyWithCount { Count: > 100 } swc) {
+				WriteLine($"Has a count over 100! {swc.Count}");
+			}
+		}
+
+		HandleSmarty(countSmarty);
+		HandleSmarty(overlordSmarty);
+
 		var smarties = new Smarty[] {
 			new SmartyWithCount("Red", 12),
 			new Smarty("Gray"),
@@ -22,11 +45,36 @@ public class PatternMatchingDemo : IDemo
 			new SmartyWithCount("Red", 9001)
 		};
 
+		WriteLine("GetSmartyOldWay ---------------------");
+
+		smarties.ToList().ForEach(s => GetSmartyOldWay(s));
+
+		void GetSmartyOldWay(Smarty s) 
+		{
+			switch(s)
+			{
+				case SmartyOverlord overlordSmarty:
+					WriteLine("Overlord smarty");
+					break;
+				case SmartyWithCount swcOver10 when swcOver10.Count > 10 && swcOver10.Color == "Red":
+					WriteLine("Has count and is red");
+					break;
+				case SmartyWithCount swcOver10 when swcOver10.Count > 10:
+					WriteLine("Has count");
+					break;
+				case Smarty normalSmarty:
+					WriteLine("Normal smarty");
+					break;
+			}
+		}
+
+		WriteLine("GetSmartyOldWay ---------------------");
+
 		string GetSmarty(SmartyWithCount swc) => swc.Count switch
 		{
 			< 10 => "A",
 			>= 10 => "B",
-			// _ => "C" //The pattern is unreachable. It has already been handled by a previous arm of the switch expression or it is impossible to match.
+			//_ => "C" //The pattern is unreachable. It has already been handled by a previous arm of the switch expression or it is impossible to match.
 		};
 
 		string GetSmartyEx1(SmartyWithCount swc) => swc switch
@@ -49,12 +97,34 @@ public class PatternMatchingDemo : IDemo
 			_ => throw new ArgumentException($"{swc} is not a valid smarty!") 
 		};
 
+		string GetSmartyEx3(Smarty swc) => swc switch
+		{
+			SmartyOverlord so => "Don't question the Smarty Overlord!",
+			SmartyWithCount { Color: "Green" } => "Green Smarties are cool!",
+			SmartyWithCount { Color: "Red", Count: > 9000 } => "Red Smarties are cool if over 9000!",
+			SmartyWithCount { Count: < 5 } smolSmarty => $"A {smolSmarty.Color}",
+			SmartyWithCount { Count: < 10 } => "B",
+			SmartyWithCount { Count: >= 10, Count: < 50 } => "B",
+			{ Color: "Gray"} => "Gray privileges!",
+			_ => throw new ArgumentException($"{swc} is not a valid smarty!") 
+		};
+
+		string GetTest(Smarty s) => s switch
+		{
+			SmartyOverlord so => "Overlord",
+			SmartyWithCount swc when swc.Count > 100 => "Count over 100",
+			{ Color: "Red" } => "Red",
+
+			_ => "Oops"
+		};
+
 		var smtsWithCount = smartiesWithCount.Select((val, idx) => (val, idx)).ToList();
-		smtsWithCount.ForEach(smt => WriteLine($"GetSmarty(s) #{smt.idx}: {GetSmarty(smt.val)}"));
-		smtsWithCount.ForEach(smt => WriteLine($"GetSmartyEx1(s) #{smt.idx}: {GetSmartyEx1(smt.val)}"));
+		smtsWithCount.ForEach(smt => WriteLine($"GetSmarty #{smt.idx}: {GetSmarty(smt.val)}"));
+		smtsWithCount.ForEach(smt => WriteLine($"GetSmartyEx1 #{smt.idx}: {GetSmartyEx1(smt.val)}"));
 		
 		var smtsNormal = smarties.Select((val, idx) => (val, idx)).ToList();
 		smtsNormal.ForEach(smt => WriteLine($"GetSmartyEx2(s) #{smt.idx}: {GetSmartyEx2(smt.val)}"));
+		smtsNormal.ForEach(smt => WriteLine($"GetSmartyEx3(s) #{smt.idx}: {GetSmartyEx3(smt.val)}"));
 		
 		WriteLine("- Logical patterns ----------------------");
 		static int GetTax(int muncipalityId) => muncipalityId switch
@@ -123,13 +193,28 @@ public class PatternMatchingDemo : IDemo
 		);
 
 		var whoOhWho1 = demoTime1 is { stockholmSyndromeFamiliar: { name: "Matthijs" }};
-		var whoOhWho2 = demoTime2 is { stockholmSyndromeFamiliar: { name: "Matthijs", customer: { customerName: "GDH" }}};
-		var whoOhWho3 = demoTime3 is { stockholmSyndromeFamiliar: { name: "Lou", customer: { customerName: "GDH" }}};
+		var whoOhWho2 = demoTime2 is { stockholmSyndromeFamiliar: { name: "Matthijs", customer: { customerName: "GDH" }}}; //nesting
+		var whoOhWho3 = demoTime3 is { stockholmSyndromeFamiliar: { name: "Lou", customer.customerName: "GDH" }}; //dot notation for nesting
 		WriteLine($"Is Matthijs working!?: {whoOhWho1}");
-		WriteLine($"Is Matthijs working for GHD!?: {whoOhWho2}");
-		WriteLine($"Is Lou working for GHD!?: {whoOhWho3}");
+		WriteLine($"Is Matthijs working for GDH!?: {whoOhWho2}");
+		WriteLine($"Is Lou working for GDH!?: {whoOhWho3}");
+
+		WriteLine("- Object deconstruction and matching ----------------------");
+
+		static decimal GetGroupTicketPriceDiscount(int groupSize, DateTime visitDate) =>
+			(groupSize, visitDate.DayOfWeek) switch
+		{
+			(<=0, _) => throw new ArgumentException( "Group size must be positive."),
+			(_, DayOfWeek.Saturday or DayOfWeek.Sunday) => 0.0m,
+			(>= 5 and < 10, DayOfWeek.Monday) => 20.0m,
+			(>= 10, DayOfWeek.Monday) => 30.0m,
+			(>= 5 and < 10, _) => 12.0m,
+			(>= 10, _) => 15.0m,
+			_ => 0.0m,
+		};
 	}
 }
+	
 
 record OneFox(OneFoxee stockholmSyndromeFamiliar);
 record OneFoxee(string name, OneFoxVictim customer);
